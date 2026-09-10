69b6252d4a9fa7db605f00de08696765827207eb
Mohamed <mohamedelibrahymy374@gmail.com>
2026-09-10 13:12:24 +0200

    #1 HTML first

---
+++ profilecard/src/routes/+page.svelte
@@ -0,0 +1,43 @@
+<header>
+  <img
+    src="src/lib/assets/mo.jpg"
+    alt="Mohamed El Ibrahymy"
+    width="150"
+    height="150"
+  />
+  <article>
+    <h1>Mohamed <span>El Ibrahymy</span></h1>
+    <p>Frontend Developer</p>
+  </article>
+</header>
+
+<main>
+  <section>
+    <h2>Over mij</h2>
+    <p>
+      Frontend developer in opleiding aan de Hogeschool van Amsterdam BLA BLA
+      BLA BLA BALB
+    </p>
+  </section>
+
+  <section>
+    <h2>Feiten</h2>
+    <ul>
+      <li></li>
+    </ul>
+  </section>
+
+  <section>
+    <h2>Contact</h2>
+    <nav>
+      <a href="https://github.com/mohamed"
+        ><article><small>github</small>github.com/mohamed</article>
+        <span>→</span></a
+      >
+      <a href="https://linkedin.com/in/mohamed"
+        ><article><small>Instagram</small>linkedin.com/in/mohamed</article>
+        <span>→</span></a
+      >
+    </nav>
+  </section>
+</main>
